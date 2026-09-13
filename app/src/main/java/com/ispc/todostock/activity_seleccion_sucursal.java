a1f368648b222d7f969e78aea6d827a95f53e654
Candelaria Barjollo <candebarjollo@gmail.com>
2026-09-13 19:08:31 -0300

    feat: agrego vecto flecha atrás

---
+++ app/src/main/java/com/ispc/todostock/activity_seleccion_sucursal.java
@@ -0,0 +1,14 @@
+package com.ispc.todostock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_seleccion_sucursal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_sucursal);
+    }
+}
